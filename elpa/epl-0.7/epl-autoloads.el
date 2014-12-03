f6227620a15646c218b112eb07b6ff479a5c843f
Simon Fromme <simonfromme@gmail.com>
2014-12-03 21:14:26 +0100

    various packaged added (bbdb, helm, haskell mode, ...)

---
+++ elpa/epl-0.7/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21610 30309 806668
+;;;;;;  537000))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
